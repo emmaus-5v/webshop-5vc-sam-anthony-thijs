d14d5ec06445709d105b5147e3caec02b5e5ba33
thijskra <112435@emmauscollege.nl>
2021-11-03 08:36:07 +0000

    cavia
    Signed-off-by: thijskra <112435@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -23,16 +23,16 @@ CREATE TABLE products (
 
 insert into products (name, description, code, price) values ('Cavia', 'Cavia is een knaagdier wat in verschillende kleuren beschikbaar is en voor een mooi degelijk prijsje.', '816905633-0', 10.5);
 
-insert into products (name, description, code, price) values ('TRIXIE GROOT BUITENVERBLIJF MET REN', 'Het Trixie Groot buitenverblijf met ren is een compleet verblijf voor uw konijn of cavia.
-Het is gemaakt van gebeitst vurenhout met stevig metalen, gepoedercoat traliewerk en bitumen dak.
-Het verblijf bestaat uit twee verdiepingen met beneden een ren en schuilplek. 
-Door de kunststof uitschuiflade is het hok makkelijk te verschonen. Er zit extra opbergruimte in het dak. Ook geschikt voor kleine leefgroepen.', '445924201-X', 249,60);
+insert into products (name, description, code, price) values ('TRIXIE GROOT BUITENVERBLIJF MET REN', 'Het Trixie Groot buitenverblijf met ren is een compleet verblijf voor uw konijn of cavia. Het is gemaakt van gebeitst vurenhout met stevig metalen, gepoedercoat traliewerk en bitumen dak.
+
+Het verblijf bestaat uit twee verdiepingen met beneden een ren en schuilplek. Door de kunststof uitschuiflade is het hok makkelijk te verschonen. Er zit extra opbergruimte in het dak. Ook geschikt voor kleine leefgroepen.', '445924201-X', 249,60);
 
 insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
 
 In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
 
 Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', '693155505-7', 13.5);
+
 insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
