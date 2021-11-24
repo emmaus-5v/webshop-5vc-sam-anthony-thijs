8e515898ef349cea74c6739372f2065ec5d4e1c5
thijskra <112435@emmauscollege.nl>
2021-11-24 08:47:30 +0000

    geoordend
    Signed-off-by: thijskra <112435@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -11,54 +11,6 @@ CREATE TABLE products (
   price NUMERIC(10, 2)
 );
 
-
-
-/*tabel goed*/
-CREATE TABLE dieren (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  soort TEXT,
-  kleur TEXT,
-  ras TEXT,
-  gewicht NUMERIC(10,2)
-);
-
-/*tabel goed*/
-CREATE TABLE accesoires (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  kleur TEXT,
-  dier_soort INTEGER,
-  accesoire_soort TEXT
-);
-
-/*tabel goed*/
-CREATE TABLE id_tabel (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-dieren.soort INTEGER,
-diersoort TEXT,
-accesoires.dier INTEGER
-);
-
-/*tabel goed*/
-CREATE TABLE producten (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  naam TEXT
-  beschrijving TEXT,
-  prijs INTEGER,
-  soort_id INTEGER
-);
-
-
-
-
---
--- populate with data
---
--- generared using
--- curl "https://api.mockaroo.com/api/910b6c20?count=100&key=90eac760" > seed.sql
---
--- want different data? check: https://www.mockaroo.com/910b6c20
---
-
 insert into products (name, description, code, price) values ('Cavia', 'Cavia is een knaagdier wat in verschillende kleuren beschikbaar is en voor een mooi degelijk prijsje.', '816905633-0', 10.5);
 
 insert into products (name, description, code, price) values ('Trixie Buitenverblijf', 'Het Trixie Groot buitenverblijf met ren is een compleet verblijf voor uw konijn of cavia. Het is gemaakt van gebeitst vurenhout met stevig metalen.
@@ -79,10 +31,62 @@ insert into products (name, description, code, price) values ('Velvet Goldmine',
 
 Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '492662523-7', 14);
 
+/*tabel goed*/
+CREATE TABLE dieren (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  soort TEXT,
+  kleur TEXT,
+  ras TEXT,
+  gewicht NUMERIC(10,2)
+);
+
 insert into dieren (soort, kleur, ras, gewicht) values ('Cavia', 'Zwart', 'Langhaar', 10);
 
-insert into producten (naam, beschrijving, prijs, soort_id) values ('Cavia Hok', 'Een cavia hok lol', 40, 1);
+/*tabel goed*/
+CREATE TABLE accesoires (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  kleur TEXT,
+  dier_soort INTEGER,
+  accesoire_soort TEXT
+);
 
 insert into accesoires (kleur, dier-id, prijs, accesoire_soort) values ('Rood', 1, 40, 'hok');
+
+/*tabel goed*/
+CREATE TABLE id_tabel (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+dieren.soort INTEGER,
+diersoort TEXT,
+accesoires.dier INTEGER
+);
+
+/*tabel goed*/
+CREATE TABLE producten (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  naam TEXT
+  beschrijving TEXT,
+  prijs INTEGER,
+  soort_id INTEGER
+);
+insert into producten (naam, beschrijving, prijs, soort_id) values ('Cavia Hok', 'Een cavia hok lol', 40, 1);
+
+
+
+
+--
+-- populate with data
+--
+-- generared using
+-- curl "https://api.mockaroo.com/api/910b6c20?count=100&key=90eac760" > seed.sql
+--
+-- want different data? check: https://www.mockaroo.com/910b6c20
+--
+
+
+
+
+
+
+
 
 
