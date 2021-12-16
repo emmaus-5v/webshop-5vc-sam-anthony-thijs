0a2b32c5095528a36a1bb3872de60ff1a3f55876
samvb5 <112301@emmauscollege.nl>
2021-12-16 12:06:40 +0000

    betere sql en api
    Signed-off-by: samvb5 <112301@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -6,7 +6,7 @@
 DROP TABLE IF EXISTS products; 
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  diersoort TEXT,
+  diersoort INTEGER,
   kleur TEXT,
   ras TEXT,
   geslacht INTEGER,
@@ -86,14 +86,20 @@ acs_id INTEGER PRIMARY KEY AUTOINCREMENT,
 acs_naam TEXT
 );
 
-insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Cavia', 'Cavia is een knaagdier wat in verschillende kleuren beschikbaar is en voor een mooi degelijk prijsje.', '816905633-0', 10.50, 'Cavia', 'Zwart', 'Langhaar', 1, 1);
-insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Konijn', 'Heeft u altijd al een schattig knaagdier gewild dan is dit uw kans voor een schattig klein dwergse langhoor.', '445924201-X', 15.60, 'Konijn', 'bruin', 'Nederlandse Hangoor Dwerg', 2, 5);
-insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Labrador','Wil de u altijd aleen Labrador dan hebben we nu een bruin cocolade labrador pup voor een mooi zacht prijsje (199 euro voor een labrador zonder vaccinaties en 600 euro met vacinaties.)', '693155505-7', 199.00, 'Hond', 'bruin', 'Labrador', 1, 12);
-insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Goudvis', 'Heeft u altijd al een vis gewild en met name een goudvis dan is dit uw kans.', '686928463-6', 4.50, 'Goudvis', 'Oranje', 'nvt', 3, 0.1);
-insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Grasparkiet', 'Heeft u nou altijd al een grasparkiet willen hebben dat kan dan nu voor een mooi zacht prijsje.', '492662523-7', 26.45, 'Grasparkiet', 'Citroengeel', 'nvt', 1, 0.4);
-insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Capybara', 'Heeft u altijd al een grote cavia gewild of dan is dit uw kans om een capybara te verkrijgen (wij hebben alle vernodigde vergunningen om hem veilig en wel naar uw te verkopen).', '2353465564-2', 696.96, 'Capybara', 'Bruin', 'nvt', 1, 40);
-insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Sprinkhaan', 'Heeft u altijd al een sprinkhaan gewild dan is dit uw kans.', '2353465564-2', 1.50, 'Sprinkhaan', 'Groen', 'nvt', 2, 0.07);
-insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Kat', 'Heeft u altijd al een schattig chagerijninge kat gewild dan is dit uw kans om een britse langhaar te verkrijgen (150 euro zonder vaccinaties en 350 euro met vacinaties.).', '2353465564-2', 150.00, 'Kat', 'Grijs', 'Britse Langhaar', 2, 10);
+DROP TABLE IF EXISTS diersoorten;
+CREATE TABLE diersoorten (
+ds_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ds_naam TEXT
+);
+
+insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Cavia', 'Cavia is een knaagdier wat in verschillende kleuren beschikbaar is en voor een mooi degelijk prijsje.', '816905633-0', 10.50, 1, 'Zwart', 'Langhaar', 1, 1);
+insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Konijn', 'Heeft u altijd al een schattig knaagdier gewild dan is dit uw kans voor een schattig klein dwergse langhoor.', '445924201-X', 15.60, 2, 'bruin', 'Nederlandse Hangoor Dwerg', 2, 5);
+insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Labrador','Wil de u altijd aleen Labrador dan hebben we nu een bruin cocolade labrador pup voor een mooi zacht prijsje (199 euro voor een labrador zonder vaccinaties en 600 euro met vacinaties.)', '693155505-7', 199.00, 3, 'bruin', 'Labrador', 1, 12);
+insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Goudvis', 'Heeft u altijd al een vis gewild en met name een goudvis dan is dit uw kans.', '686928463-6', 4.50, 4, 'Oranje', 'nvt', 3, 0.1);
+insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Grasparkiet', 'Heeft u nou altijd al een grasparkiet willen hebben dat kan dan nu voor een mooi zacht prijsje.', '492662523-7', 26.45, 5, 'Citroengeel', 'nvt', 1, 0.4);
+insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Capybara', 'Heeft u altijd al een grote cavia gewild of dan is dit uw kans om een capybara te verkrijgen (wij hebben alle vernodigde vergunningen om hem veilig en wel naar uw te verkopen).', '2353465564-2', 696.96, 6, 'Bruin', 'nvt', 1, 40);
+insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Sprinkhaan', 'Heeft u altijd al een sprinkhaan gewild dan is dit uw kans.', '2353465564-2', 1.50, 7, 'Groen', 'nvt', 2, 0.07);
+insert into products (name, description, code, price, diersoort, kleur, ras, geslacht, gewicht) values ('Kat', 'Heeft u altijd al een schattig chagerijninge kat gewild dan is dit uw kans om een britse langhaar te verkrijgen (150 euro zonder vaccinaties en 350 euro met vacinaties.).', '2353465564-2', 150.00, 8, 'Grijs', 'Britse Langhaar', 2, 10);
 
 
 insert into accesoires (a_naam, a_kleur, soort) values ('caviahok', 'bruinhout', 1);
@@ -165,7 +171,14 @@ insert into accesoiresoorten (acs_naam) values ('verblijf');
 insert into accesoiresoorten (acs_naam) values ('speeltje');
 insert into accesoiresoorten (acs_naam) values ('kleding');
 
-
+insert into diersoorten (ds_naam) values ('cavia');
+insert into diersoorten (ds_naam) values ('konijn');
+insert into diersoorten (ds_naam) values ('hond');
+insert into diersoorten (ds_naam) values ('goudvis');
+insert into diersoorten (ds_naam) values ('grasparkiet');
+insert into diersoorten (ds_naam) values ('capybara');
+insert into diersoorten (ds_naam) values ('sprinkhaan');
+insert into diersoorten (ds_naam) values ('kat');
 
 
 --
